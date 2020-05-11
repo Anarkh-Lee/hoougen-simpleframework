fda135a192bb6c211e4dc01d1e807bf6227ce549
hooyot <36752888+hooyot@users.noreply.github.com>
2020-05-11 08:51:17 +0800

    create the project structure
    Just practice. Just practice. Just practice.
    It's so important that it should be repeated for three times.

---
+++ hoougen-zuul/src/main/java/com/hoougen/simpleframework/zuul/HoougenZuulApplication.java
@@ -0,0 +1,13 @@
+package com.hoougen.simpleframework.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoougenZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoougenZuulApplication.class, args);
+	}
+
+}
